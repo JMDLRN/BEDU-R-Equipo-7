3295fafa16c4be27bb5cd0d754f6416f434267fd
JMDLRN <manuel.delarosa.nava@bsci.com>
2022-12-18 13:15:18 -0600

    Commit inicial

---
+++ Proyecto_EQ7_Punto6.R
@@ -0,0 +1,19 @@
+"6 Escribe tu análisis en un archivo README.MD y tu código en un script de R y
+publica ambos en un repositorio de Github."
+#Librería RStudioAPI
+library(rstudioapi)
+
+#Commit inicial
+myTerm <- terminalCreate(caption = "GIT", shellType = "win-git-bash")
+terminalSend(myTerm, "git config --global user.name 'JMDLRN'\n")
+terminalSend(myTerm, "git config --global user.email 'manuel.delarosa.nava@bsci.com'\n")
+terminalSend(myTerm, "git status\n")
+terminalSend(myTerm, "git add README.md\n")
+terminalSend(myTerm, "git add Proyecto_EQ7.R\n")
+terminalSend(myTerm, "git commit -m 'Commit inicial'\n")
+terminalSend(myTerm, "git branch -M main\n")
+terminalSend(myTerm, "git remote add origin _\n")
+terminalSend(myTerm, "git push -u origin main\n")
+
+#Cambios desde Github
+terminalSend(myTerm, "git pull origin main\n")
